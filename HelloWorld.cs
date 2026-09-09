aa0d24a24a3fdeb015352c4d56b0c4cd06cd2838
Oscar-adler <kapafilipp@gmail.com>
2026-09-09 13:55:21 +1200

    Первая версия проги

---
+++ HelloWorld.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Notes_service
+{
+	public class Program
+	{
+		class Note
+		{
+			string name;
+			string description;
+			int priorites;
+
+			public Note(int priorites, string name) 
+			{
+				this.priorites = priorites;
+				this.name = name;
+			}
+
+			public Note(int priorites, string name, string description)
+			{
+				this.priorites = priorites;
+				this.name = name;
+				this.description = description;
+			}
+
+			public void show_note() 
+			{
+				Console.WriteLine($"{name}\n{description}\nПриоритет: {priorites}");
+			}
+		}
+		public static void Main(string[] args)
+		{
+			Note obj1 = new Note(1,"Заметка 1","Моя первая Заметка");
+			obj1.show_note();
+		}
+	}
+}
